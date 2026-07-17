3d12e5f59f6c53ba3779e1d5e45231ad791d300b
RamezAtallah-9r <ramez.atallah@hotmail.com>
2026-07-17 10:12:27 +0300

    first project

---
+++ life/src/test/java/com/axsos/Life/LifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.Life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
